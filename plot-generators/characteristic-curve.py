f115671a95ce45dc5fc0e58becd0d8097e27b73b
David Brewster <brewcrew5498@outlook.com>
2024-06-06 19:02:30 -0400

    characteristic curve single

--- plot-generators/characteristic-curve.py
+++ plot-generators/characteristic-curve.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 import matplotlib.pyplot as plt
 import networkx as nx
 import numpy as np
@@ -23,19 +24,20 @@ plt.rcParams.update({
 })
 
 N = int(os.getenv("N"))
-assert is_perfect_square(N), N
+# assert is_perfect_square(N), N
 
 NUM_WORKERS = int(os.getenv("NUM_WORKERS"))
 NUM_SIMULATIONS = int(os.getenv("NUM_SIMULATIONS"))
-THRESHOLD = int(os.getenv("THRESHOLD"))
-ABSORBED_SIMPSONS_INDEX = 0
+STAT_TO_CALCULATE = os.getenv("STAT_TO_CALCULATE").strip().lower()
+MODE = os.getenv("MODE").strip().lower()
 GRAPH_GENERATORS = [
   GraphGenerator(nx.complete_graph, 'complete'),
-  GraphGenerator(conjoined_star_graph, 'conjoined star'),
-  GraphGenerator(nx.cycle_graph, 'cycle'),
-  GraphGenerator(square_periodic_grid, 'square periodic grid'),
-  GraphGenerator(star_graph, 'star'),
+  # GraphGenerator(conjoined_star_graph, 'conjoined star'),
+  # GraphGenerator(nx.cycle_graph, 'cycle'),
+  # GraphGenerator(square_periodic_grid, 'square periodic grid'),
+  # GraphGenerator(star_graph, 'star'),
 ]
+
 
 def simpsons_index(S: Dict[int, Set[Any]], n: int):
   """complement"""
@@ -44,27 +46,109 @@ def simpsons_index(S: Dict[int, Set[Any]], n: int):
     for locations in S.values()
   ) / (n*(n-1)) 
 
+def spatial_diversity(S: Dict[int, Set[Any]], G: nx.Graph) -> float:
+  # S is type to set of locations.
+  S_rev = {v: k
+    for k, vs in S.items()
+    for v in vs
+  }
+  return sum(
+    int(S_rev[u] != S_rev[v])
+    for (u, v) in G.edges()
+  ) / G.number_of_edges()
 
 from collections import defaultdict
 
-def simulate(graph_generator: GraphGenerator, n: int) -> Tuple[str, Dict[int, Tuple[float, int]]]:
+def num_types_left(S: Dict[int, Set[Any]]):
+  return sum(
+    int(len(locations) > 0)
+    for locations in S.values()
+  )
+
+@dataclass
+class Stats:
+  ABSORBED_SIMPSONS_INDEX: ClassVar[int] = 0
+  ABSORBED_NUM_TYPES_LEFT: ClassVar[int] = 1
+  ABSORBED_SPATIAL_DIVERSITY: ClassVar[int] = 0
+
+  simpsons_index: float = None
+  num_types_left: float = None
+  spatial_diversity: Optional[float] = None
+  num_samples: Optional[float] = None
+  trial: Optional[int] = None
+
+T = TypeVar("T")
+def calculate_average(dataset: List[T], extractor: Callable[[T], float], pad_value: float):
+  return np.mean([extractor(datum) for datum in dataset] + [pad_value] * max(NUM_SIMULATIONS-len(dataset), 0))
+
+
+def simulate_multiple(graph_generator: GraphGenerator, n: int) -> Tuple[str, Dict[int, Stats]]:
   logger.info((graph_generator.name, n))
-  D_at_steps: DefaultDict[int, List[float]] = defaultdict(list)
+  stats_at_steps: DefaultDict[int, List[Stats]] = defaultdict(list)
   if (G := graph_generator.build_graph(n)) is None: return None
 
   assert len(G) == n, (graph_generator.name, n)
   for _ in range(NUM_SIMULATIONS):
     for steps, S in trial_absorption_time(G, interactive=True):
-      D_at_steps[steps].append(simpsons_index(S, N))
+      stats_at_steps[steps].append(
+        Stats(
+          simpsons_index=simpsons_index(S, N) if STAT_TO_CALCULATE == 'simpsons_index' else None,
+          spatial_diversity=spatial_diversity(S, G) if STAT_TO_CALCULATE == 'spatial_diversity' else None,
+          num_types_left=num_types_left(S) if STAT_TO_CALCULATE == 'num_types_left' else None,
+        )
+      )
 
-  avg_D_at_steps = {
-    steps: (np.mean(Ds + [ABSORBED_SIMPSONS_INDEX] * max(NUM_SIMULATIONS-len(Ds), 0)), len(Ds))
-    for steps, Ds in D_at_steps.items()
+  avg_stat_at_steps = {
+    steps: Stats(
+      simpsons_index=calculate_average(dataset=stats, extractor=lambda stat: stat.simpsons_index, pad_value=Stats.ABSORBED_SIMPSONS_INDEX) if STAT_TO_CALCULATE == 'simpsons_index' else None,
+      num_types_left=calculate_average(dataset=stats, extractor=lambda stat: stat.num_types_left, pad_value=Stats.ABSORBED_NUM_TYPES_LEFT) if STAT_TO_CALCULATE == 'num_types_left' else None,
+      spatial_diversity=calculate_average(dataset=stats, extractor=lambda stat: stat.spatial_diversity, pad_value=Stats.ABSORBED_SPATIAL_DIVERSITY) if STAT_TO_CALCULATE == 'spatial_diversity' else None,
+      num_samples=len(stats),
+    )
+    for steps, stats in stats_at_steps.items()
   }
 
-  return (graph_generator.name, avg_D_at_steps)
+  return (graph_generator.name, avg_stat_at_steps)
+
+def process_multiple(datum: Tuple[str, Dict[int, Stats]]):
+  data = []
+  graph_family_name, avg_stats_at_steps = datum
+  for steps, stat in avg_stats_at_steps.items():
+    data.append((graph_family_name, steps, stat.trial, stat.simpsons_index, stat.num_samples, stat.num_types_left, stat.spatial_diversity))
+  return data
+
+def simulate_single(graph_generator: GraphGenerator, n: int) -> Tuple[str, Dict[int, List[Stats]]]:
+  logger.info((graph_generator.name, n))
+  stats_at_steps: DefaultDict[int, List[Stats]] = defaultdict(list)
+  if (G := graph_generator.build_graph(n)) is None: return None
+
+  assert len(G) == n, (graph_generator.name, n)
+  for trial in range(NUM_SIMULATIONS):
+    for steps, S in trial_absorption_time(G, interactive=True):
+      stats_at_steps[steps].append(
+        Stats(
+          simpsons_index=simpsons_index(S, N) if STAT_TO_CALCULATE == 'simpsons_index' else None,
+          spatial_diversity=spatial_diversity(S, G) if STAT_TO_CALCULATE == 'spatial_diversity' else None,
+          num_types_left=num_types_left(S) if STAT_TO_CALCULATE == 'num_types_left' else None,
+          trial=trial,
+        )
+      )
+
+  return (graph_generator.name, stats_at_steps)
+
+def process_single(datum: Tuple[str, Dict[int, List[Stats]]]):
+  data = []
+  graph_family_name, stats_at_steps = datum
+  for steps, stats in stats_at_steps.items():
+    for stat in stats:
+      data.append((graph_family_name, steps, stat.trial, stat.simpsons_index, stat.num_samples, stat.num_types_left, stat.spatial_diversity))
+  return data
+
 
 def compute() -> pd.DataFrame:
+  process = process_single if MODE == 'single' else process_multiple if MODE == 'multiple' else None
+  simulate = simulate_single if MODE == 'single' else simulate_multiple if MODE == 'multiple' else None
+
   data = []
   with Pool(NUM_WORKERS) as p:
     for datum in p.starmap(simulate, (
@@ -72,18 +156,17 @@ def compute() -> pd.DataFrame:
       for graph_generator in GRAPH_GENERATORS
     )):
       if not datum: continue
-      graph_family_name, avg_D_at_steps = datum
-      for steps, (avg_D, num_samples) in avg_D_at_steps.items():
-        data.append((graph_family_name, steps, avg_D, num_samples))
+      data.extend(process(datum))
+
   
-  df = pd.DataFrame(data, columns=['graph_family', 'time', 'average_simpsons_index', 'num_samples'])
+  df = pd.DataFrame(data, columns=['graph_family', 'time', 'trial', 'simpsons_index', 'num_samples', 'num_types_left', 'spatial_diversity'])
   return df
 
-def draw(df: pd.DataFrame):
+def draw_multiple(df: pd.DataFrame):
   plot = sns.scatterplot(
-    df[df['num_samples'] >= THRESHOLD],
+    df,
     x='time',
-    y='average_simpsons_index',
+    y='spatial_diversity',
     hue='graph_family',
     linewidth=0,
     alpha=1.0
@@ -92,52 +175,54 @@ def draw(df: pd.DataFrame):
     # dashes=False,
     # sort=True,
     # markersize=20,
-    # opacity='num_samples',
-  )
-  sns.scatterplot(
-    df[df['num_samples'] < THRESHOLD],
-    ax=plot,
-    x='time',
-    y='average_simpsons_index',
-    hue='graph_family',
-    linewidth=0,
-    legend=False,
-    alpha=0.05,
-    # style='graph_family',
-    # markers=True,
-    # dashes=False,
-    # sort=True,
-    # markersize=20,
-    # opacity='num_samples',
   )
 
   plt.xlabel(r'Time, $T$')
-  plt.ylabel(r"Average Simpon's Index, $\overline{D}$")
-  # plt.xscale('squareroot')
-  # plt.yscale('squareroot')
-  # plt.xscale('log')
+  plt.ylabel(r"Average Spatial Diversity, $\overline{D}_s$")
+  plt.xscale('log')
   # plt.yscale('log')
-  plt.xlim(left=0)
-  # plt.xscale('function', functions=(lambda x: print(x) or np.log(1/(1-np.log((2+x)))-1), lambda x: np.exp(1-(1/(np.exp(x)+1)))-2))
-  plt.title(f'{N=} {NUM_SIMULATIONS=} {THRESHOLD=}')
-  #get legend and change stuff
-  # handles, lables = plot.get_legend_handles_labels()
-  # for h in handles:
-  #   h.set_markersize(20)
-
-  # replace legend using handles and labels from above
-  # lgnd = plt.legend(handles, lables, loc='upper left', borderaxespad=0.2, title='graph family')
-  # plt.legend()
-  # plt.tight_layout()
+  # plt.xlim(left=0)
+  plt.title(f'{N=} {NUM_SIMULATIONS=}')
   dpi = 300
   width, height = 2*np.array([3024, 1964])
   fig = plot.get_figure()
   fig.set_size_inches(*(width/dpi, height/dpi))
-  fig.savefig('plots/characteristic-curve.png', dpi=dpi)
+  fig.savefig('plots/characteristic-curve-multiple.png', dpi=dpi)
 
+def draw_single(df: pd.DataFrame):
+  assert len(GRAPH_GENERATORS) == 1, GRAPH_GENERATORS
+  graph_family = GRAPH_GENERATORS[0].name
+  plot = sns.lineplot(
+    df,
+    x='time',
+    y='spatial_diversity',
+    hue='trial',
+    # markers=True,
+    legend=False,
+    linestyle='--',
+    alpha=0.5,
+    # style='graph_family',
+    # dashes=True,
+    # sort=True,
+    marker="o",
+    markersize=7,
+  )
+
+  plt.xlabel(r'Time, $T$')
+  plt.ylabel(r"Spatial Diversity, $D_s$")
+  # plt.xscale('log')
+  # plt.yscale('log')
+  # plt.xlim(left=0)
+  plt.title(f'{graph_family=} {N=} {NUM_SIMULATIONS=}')
+  dpi = 300
+  width, height = 2*np.array([3024, 1964])
+  fig = plot.get_figure()
+  fig.set_size_inches(*(width/dpi, height/dpi))
+  fig.savefig('plots/characteristic-curve-single.png', dpi=dpi)
 
 if __name__ == '__main__':
+  if MODE == 'single': assert len(GRAPH_GENERATORS) == 1, [gg.name for gg in GRAPH_GENERATORS]
   df = compute() 
   # save_data(df)
+  draw = draw_single if MODE == 'single' else draw_multiple if MODE == 'multiple' else None
   draw(df)
-  
