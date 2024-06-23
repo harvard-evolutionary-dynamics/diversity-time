a43792f94e1dc4e2b76a512bf362ba665733ac33
David Brewster <brewcrew5498@outlook.com>
2024-06-23 08:30:20 -0400

    update

--- plot-generators/erdos-renyi.py
+++ plot-generators/erdos-renyi.py
@@ -1,35 +1,45 @@
+import matplotlib.cm as cm
 import matplotlib.pyplot as plt
 import networkx as nx
 import numpy as np
 import pandas as pd
 import seaborn as sns
+import os
 
 from utils import sample
 from absorption import trial_absorption_time
 from typing import *
 from multiprocessing import Pool
+from pathlib import Path
+from dotenv import load_dotenv
+from customlogger import logger
+
+load_dotenv()
 
 plt.rcParams.update({
   "text.usetex": True,
   "font.family": "Helvetica"
 })
 
+N = int(os.getenv("N"))
+NUM_WORKERS = int(os.getenv("NUM_WORKERS"))
+NUM_SIMULATIONS = int(os.getenv("NUM_SIMULATIONS"))
+NUM_INTERVALS = int(os.getenv("NUM_INTERVALS"))
+USE_EXISTING_DATA = os.getenv("USE_EXISTING_DATA", default='false').lower() not in ('false', '0')
+OVERWRITE = os.getenv("OVERWRITE", default='false').lower() not in ('false', '0')
+ERDOS_RENYI_DATA_FILE = Path(os.environ["ERDOS_RENYI_DATA_FILE"])
+DRAW = os.getenv("DRAW", default='false').lower() not in ('false', '0')
 
 
 def simulate(n: int, p: float):
-  print('start', n, p)
-  samples = []
-  TRIALS = 1_000
+  logger.info(('start', n, p))
   abs_times = []
-  for _ in range(TRIALS):
+  for _ in range(NUM_SIMULATIONS):
     while not nx.is_connected(G := nx.erdos_renyi_graph(n, p)): ...
     abs_time = trial_absorption_time(G)
     abs_times.append(abs_time)
-    # samples.append((n, p, abs_time))
-  print('end', n, p)
-  return [(n, p, np.mean(abs_times))] # samples
-
-NUM_WORKERS = 16
+  # print('end', n, p)
+  return [(n, p, np.mean(abs_times))]
 
 def draw(N):
   data = []
@@ -37,31 +47,30 @@ def draw(N):
     for datum in p.starmap(simulate, (
       (n, p)
       for n in range(2, N+1)
-      for p in (px for px in np.linspace(0, 1, 10*N) if px >= 2*np.log(N)/N)
+      for p in (px for px in np.linspace(0, 1, NUM_INTERVALS) if px >= 2*np.log(N)/N)
     )):
       if datum:
         data.extend(datum)
 
-  import matplotlib.cm as cm
   df = pd.DataFrame(data, columns=['number_of_nodes', 'probability', 'absorption_time'])
   plot = sns.lineplot(
     df,
     x='number_of_nodes',
     y='absorption_time',
     hue='probability',
-    palette=cm.viridis,
+    palette=cm.viridis_r,
     # style='probability',
     marker='o',
     sort=True,
     markersize=10,
-    legend=False
+    legend=False,
   )
 
 
-  plt.colorbar(cm.ScalarMappable(cmap=cm.viridis), label=r'Probability, $p$')
+  plt.colorbar(cm.ScalarMappable(cmap=cm.viridis_r), label=r'Probability, $p$')
 
   plt.xlabel(r'Number of nodes, $N$')
-  plt.ylabel(r'Absorption time, $T$')
+  plt.ylabel(r'Average Absorption time, $T$')
   plt.xscale('log')
   plt.yscale('log')
   #get legend and change stuff
@@ -78,5 +87,4 @@ def draw(N):
   fig.savefig('plots/erdos-renyi.png', dpi=dpi)
 
 if __name__ == '__main__':
-  N = 15
   draw(N)
